c665be3984dc99afa6b4b93d5443f67e58864e50
Cody Switzer <cswitzer85@gmail.com>
2020-03-05 10:59:00 -0600

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -8,7 +8,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      console.log(req.user + " user is logged in!")
+      console.log(req.user + " user is logged in!");
       res.redirect("/members");
     }
     // res.sendFile(path.join(__dirname, "../public/signup.html"));
