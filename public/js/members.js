fee1393c43101e0e6332689b07f15c702293f751
Cody Switzer <cswitzer85@gmail.com>
2020-03-06 10:15:34 -0600

    updated member page render

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
+  // Gets user e-mail and renders everything in front of @ to the welcome section of the page while rendering email in email section.
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+    var emailString = data.email;
+    var emailSplitArry = emailString.split("@");
+    $(".member-name").text(emailSplitArry[0]);
+    $(".member-email").text(data.email);
   });
 });
